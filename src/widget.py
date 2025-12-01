83c7fd161cbf23d8f94d38955591ce4fa0c6e5e8
ampest <vlad_voice@mail.ru>
2025-12-01 23:29:23 +1200

    созданы функции маскировки

--- src/widget.py
+++ src/widget.py
@@ -3,6 +3,22 @@ def mask_account_card(card_info: str) -> str:
 
 
 
+    parts = card_info.rsplit(' ',1)
+    type_name = parts[0]
+    number = parts[1]
+
+
+    if type_name == "Счет":
+        return type_name + " " + "**" + number[-4:]
+    else:
+        masked = f"{number[0:4]} {number[4:6]}** **** {number[-4:]}"
+        return type_name + " " + masked
+
+
+
+
+
+
 
 
 
