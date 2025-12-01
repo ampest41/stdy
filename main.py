64601a78c47d0c4d58bdb162184b2ed2dc4f7c25
ampest <vlad_voice@mail.ru>
2025-12-01 22:49:38 +1200

    заявлены функции

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-from src.masks import get_mask_account
-from src.masks import get_mask_card_number
+#from src.masks import get_mask_account
+#from src.masks import get_mask_card_number
 
-card_number: str = input()
+#card_number: str = input()
 
-card_number = get_mask_card_number(card_number)
-print(card_number)
+#card_number = get_mask_card_number(card_number)
+#print(card_number)
 
 
-account: str = input()
+#account: str = input()
 
-account_number = get_mask_account(account)
-print(account_number)
+#account_number = get_mask_account(account)
+#print(account_number)
+
