cc99b369a75011d25341ca1361182df01c6955dc
ampest <vlad_voice@mail.ru>
2025-12-01 20:54:32 +1200

    end commit

---
+++ src/masks.py
@@ -0,0 +1,17 @@
+def get_mask_card_number(card_number: str) -> str:
+    """функция маскирует номер карты"""
+
+    masked = card_number[0:6] + "******" + card_number[-4:]
+
+    blocks = []
+
+    for i in range(0, len(masked), 4):
+        block = masked[i: i + 4]
+        blocks.append(block)
+    return " ".join(blocks)
+
+
+def get_mask_account(account: str) -> str:
+    """функция маскирует номер счета"""
+
+    return "**" + account[-4:]
